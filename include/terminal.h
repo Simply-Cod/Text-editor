53afc3bc04a79b648b7b788129ad045ddb8aef19
Simply-Cod <simply.cod.92@gmail.com>
2026-02-02 05:37:38 +0100

    Add Terminal struct and raw mode functions

--- include/terminal.h
+++ include/terminal.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#include <termios.h>
+
+typedef struct Terminal {
+    struct termios old_term;
+    struct termios raw_term;
+}Terminal;
+
+
+int TerminalEnableRaw(Terminal *terminal);
+int TerminalDisableRaw(Terminal *terminal);
