d090fba75e07a6053f1c0184bc076be36d55514f
Simply-Cod <simply.cod.92@gmail.com>
2026-02-02 05:36:14 +0100

    Add input constants and enum for input modes

--- include/input.h
+++ include/input.h
@@ -1,1 +1,15 @@
+#pragma once
 
+#define ESC 27
+
+#define UP      1000
+#define DOWN    1001
+#define RIGHT   1002
+#define LEFT    1003
+
+enum InputMode {
+    NORMAL,
+    INSERT,
+};
+
+unsigned int readInput();
